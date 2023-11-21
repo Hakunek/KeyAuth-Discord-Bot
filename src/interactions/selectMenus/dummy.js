e9f486826d74818326917370035623020dcf4cf5
Hakunek <hakunekpl@gmail.com>
2023-11-21 21:51:38 +0100

    added dummy.js files
    added dummy.js files so that all directories are created
    
    some files include suffix `-` in case where they could have impact on running bot

---
+++ src/interactions/selectMenus/dummy.js
@@ -0,0 +1,8 @@
+/** @type {import("../../types/typesDiscord.mjs").AnySelectMenuInteraction} */
+export default {
+    name: "dummy",
+    execute: async (interaction, client) => {}
+    /* 
+        This file does nothing and is here only for the purpose of creating the directory
+     */
+};
